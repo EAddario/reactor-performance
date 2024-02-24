b232d5cf06220805039a7ae18fa47e98723b85d8
Ed Addario <ed.addario@paytrix.io>
2024-02-24 16:01:00 +0000

    Initial commit

---
+++ blocking-service/src/main/java/org/addario/blockingservice/BlockingServiceApplication.java
@@ -0,0 +1,13 @@
+package org.addario.blockingservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlockingServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BlockingServiceApplication.class, args);
+    }
+
+}
---
+++ blocking-service/src/test/java/org/addario/blockingservice/BlockingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.addario.blockingservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlockingServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
