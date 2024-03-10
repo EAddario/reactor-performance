5a886e67f67e14725e760d74247513695f100774
Ed Addario <ed.addario@paytrix.io>
2024-03-10 12:44:49 +0000

    Refactor FibonacciLong and add FibonacciMono

--- reactive-service/src/main/java/org/addario/reactiveservice/FibonacciController.java
+++ reactive-service/src/main/java/org/addario/reactiveservice/FibonacciController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Mono;
+import reactor.core.scheduler.Schedulers;
 
 import java.util.Random;
 
@@ -14,16 +15,35 @@ import java.util.Random;
 @RequestMapping("/fibonacci") // http://localhost:8081/fibonacci
 public class FibonacciController {
     // http://localhost:8081/fibonacci
-    @GetMapping(path="/{num}", produces = MediaType.TEXT_PLAIN_VALUE)
-    public Mono<ResponseEntity<String>> getFibonacci(@PathVariable Long num) {
-        Mono<String> result = Mono.just(String.format("%,d", Fibonacci.calculate(num)));
+    @GetMapping(path="/long/{num}", produces = MediaType.TEXT_PLAIN_VALUE)
+    public Mono<ResponseEntity<String>> getBlockingFibonacci(@PathVariable Long num) {
+        Mono<String> result = Mono.fromSupplier(() -> String.format("%,d", FibonacciLong.calculate(num)))
+                .subscribeOn(Schedulers.boundedElastic());
         return result.map(ResponseEntity::ok).defaultIfEmpty(ResponseEntity.notFound().build());
     }
 
-    @GetMapping(path="/random", produces = MediaType.TEXT_PLAIN_VALUE)
+    @GetMapping(path="/long/random", produces = MediaType.TEXT_PLAIN_VALUE)
     public Mono<ResponseEntity<String>> getRandom() {
-        Random rnd = new Random();
-        Mono<String> result = Mono.just(String.format("%,d", Fibonacci.calculate(rnd.nextLong(40))));
+        var rnd = new Random().nextLong(40);
+        Mono<String> result = Mono.fromSupplier(() -> String.format("%,d", FibonacciLong.calculate(rnd)))
+                .subscribeOn(Schedulers.boundedElastic());
+        return result.map(ResponseEntity::ok).defaultIfEmpty(ResponseEntity.notFound().build());
+    }
+
+    @GetMapping(path="/mono/{num}", produces = MediaType.TEXT_PLAIN_VALUE)
+    public Mono<ResponseEntity<String>> getNonBlockingFibonacci(@PathVariable Long num) {
+        Mono<String> result = FibonacciMono.calculate(num)
+                .map(v -> String.format("%,d", v))
+                .subscribeOn(Schedulers.boundedElastic());
+        return result.map(ResponseEntity::ok).defaultIfEmpty(ResponseEntity.notFound().build());
+    }
+
+    @GetMapping(path="/mono/random", produces = MediaType.TEXT_PLAIN_VALUE)
+    public Mono<ResponseEntity<String>> getNonBlockingRandom() {
+        var rnd = new Random().nextLong(40);
+        Mono<String> result = FibonacciMono.calculate(rnd)
+                .map(v -> String.format("%,d", v))
+                .subscribeOn(Schedulers.boundedElastic());
         return result.map(ResponseEntity::ok).defaultIfEmpty(ResponseEntity.notFound().build());
     }
 }
