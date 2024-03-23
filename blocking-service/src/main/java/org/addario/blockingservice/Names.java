7e9668c9e9bb954cdb47477f9274c2a41425a4e1
Ed Addario <ed.addario@paytrix.io>
2024-03-23 18:02:55 +0000

    Add Names class

---
+++ blocking-service/src/main/java/org/addario/blockingservice/Names.java
@@ -0,0 +1,33 @@
+package org.addario.blockingservice;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Random;
+import java.util.stream.IntStream;
+
+public class Names {
+    static Random r = new Random();
+    static List<String> names = List.of("Ahmad", "Ankush", "Chiamaka", "Chris", "Daniel", "David", "Ed", "Faris",
+            "Guilherme", "Ian", "Illia", "Jędrzej", "Jonathan", "Leo", "Mark", "Mic", "Nicola", "Pato", "Pedro", "Sudi");
+
+    public static String getName(int count) {
+        var namesList = IntStream.range(0, count).mapToObj(__ -> names.get(r.nextInt(names.size()))).toList();
+
+        return findName(namesList);
+    }
+
+    private static String findName(List<String> namesList) {
+        Map<String, Long> counts = new HashMap<>();
+
+        for (String name : namesList) {
+                counts.compute(name, (__, c) -> c == null ? 1L : c + 1);
+        }
+
+        return counts.entrySet()
+                .stream()
+                .max(Map.Entry.comparingByValue())
+                .get()
+                .getKey();
+    }
+}
---
+++ reactive-service/src/main/java/org/addario/reactiveservice/Names.java
@@ -0,0 +1,49 @@
+package org.addario.reactiveservice;
+
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+import reactor.core.scheduler.Schedulers;
+import reactor.math.MathFlux;
+import reactor.util.function.Tuple2;
+import reactor.util.function.Tuples;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Random;
+import java.util.function.Function;
+import java.util.stream.IntStream;
+
+public class Names {
+    static Random r = new Random();
+    static List<String> names = List.of("Ahmad", "Ankush", "Chiamaka", "Chris", "Daniel", "David", "Ed", "Faris",
+            "Guilherme", "Ian", "Illia", "Jędrzej", "Jonathan", "Leo", "Mark", "Mic", "Nicola", "Pato", "Pedro", "Sudi");
+
+    public static Mono<String> getName(int count) {
+        var namesList = IntStream.range(0, count).mapToObj(__ -> names.get(r.nextInt(names.size()))).toList();
+
+        var finalCounts = Flux.fromIterable(namesList)
+                .buffer(Runtime.getRuntime().availableProcessors())
+                .flatMap(Names::processBatch)
+                .reduce(new HashMap<>(), Names::mergeIntermediateCount)
+                .flatMapIterable(HashMap::entrySet);
+
+        return MathFlux.max(finalCounts, Map.Entry.comparingByValue())
+                .map(Map.Entry::getKey);
+                //.block();
+    }
+
+    private static HashMap<String, Long> mergeIntermediateCount(HashMap<String, Long> acc, Map<String, Long> intermediateResult) {
+        intermediateResult.forEach((name, intermediateCount) -> acc.merge(name, intermediateCount, Long::sum));
+        return acc;
+    }
+
+    private static Mono<Map<String, Long>> processBatch(List<String> batch) {
+
+        return Flux.fromIterable(batch)
+                .groupBy(Function.identity())
+                .flatMap(group -> group.count().map(count -> Tuples.of(group.key(), count)))
+                .collectMap(Tuple2::getT1, Tuple2::getT2)
+                .subscribeOn(Schedulers.boundedElastic());
+    }
+}
