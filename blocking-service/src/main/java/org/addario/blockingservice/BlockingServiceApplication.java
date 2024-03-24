d801b242e715c033385dd46d27048910378f0420
Ed Addario <ed.addario@paytrix.io>
2024-03-24 10:43:44 +0000

    Minor cosmetic changes

--- blocking-service/src/main/java/org/addario/blockingservice/BlockingServiceApplication.java
+++ blocking-service/src/main/java/org/addario/blockingservice/BlockingServiceApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class BlockingServiceApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(BlockingServiceApplication.class, args);
     }
-
 }
--- blocking-service/src/main/java/org/addario/blockingservice/Fibonacci.java
+++ blocking-service/src/main/java/org/addario/blockingservice/Fibonacci.java
@@ -3,7 +3,6 @@ package org.addario.blockingservice;
 public class Fibonacci {
     //Highly inefficient Fibonacci algorithm used to simulate service load
     public static Long calculate(Long num) {
-
         if (num == 1) return 1L;
         if (num < 1) return 0L;
 
