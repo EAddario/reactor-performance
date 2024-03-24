b13b564ff419d23c5f8a456eb77e89bc5e13ee24
Ed Addario <ed.addario@paytrix.io>
2024-03-24 10:43:16 +0000

    Refactor by renaming classes

--- blocking-service/src/main/java/org/addario/blockingservice/BlockingServiceController.java
+++ blocking-service/src/main/java/org/addario/blockingservice/BlockingServiceController.java
@@ -5,25 +5,32 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Random;
 
 @RestController
-@RequestMapping("/fibonacci") // http://localhost:8080/fibonacci
-public class FibonacciController {
-    // http://localhost:8080/fibonacci
-    @GetMapping(path="/long/{num}", produces = MediaType.TEXT_PLAIN_VALUE)
+// http://localhost:8080/
+public class BlockingServiceController {
+    @GetMapping(path="/fibonacci/{num}", produces = MediaType.TEXT_PLAIN_VALUE)
     public ResponseEntity<String> getFibonacci(@PathVariable Long num) {
         String result = String.format("%,d", Fibonacci.calculate(num));
         return new ResponseEntity<>(result, HttpStatus.OK);
     }
 
-    @GetMapping(path="/long/random", produces = MediaType.TEXT_PLAIN_VALUE)
+    @GetMapping(path="/fibonacci/random", produces = MediaType.TEXT_PLAIN_VALUE)
     public ResponseEntity<String> getRandom() {
         Random rnd = new Random();
         String result = String.format("%,d", Fibonacci.calculate(rnd.nextLong(40)));
+        return new ResponseEntity<>(result, HttpStatus.OK);
+    }
+
+    @GetMapping(path="/name/{num}", produces = MediaType.TEXT_PLAIN_VALUE)
+    public ResponseEntity<String> getName(@PathVariable int num) {
+        num = num < 1 ? 1 : num;
+        num = num > 1_000_000_000 ? 1_000_000_000 : num;
+        String result = Names.getName(num);
+
         return new ResponseEntity<>(result, HttpStatus.OK);
     }
 }
--- reactive-service/src/main/java/org/addario/reactiveservice/ReactiveServiceController.java
+++ reactive-service/src/main/java/org/addario/reactiveservice/ReactiveServiceController.java
@@ -4,7 +4,6 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Mono;
 import reactor.core.scheduler.Schedulers;
@@ -12,38 +11,49 @@ import reactor.core.scheduler.Schedulers;
 import java.util.Random;
 
 @RestController
-@RequestMapping("/fibonacci") // http://localhost:8081/fibonacci
-public class FibonacciController {
-    // http://localhost:8081/fibonacci
+// http://localhost:8081/
+public class ReactiveServiceController {
     @GetMapping(path="/long/{num}", produces = MediaType.TEXT_PLAIN_VALUE)
-    public Mono<ResponseEntity<String>> getBlockingFibonacci(@PathVariable Long num) {
+    public Mono<ResponseEntity<String>> getLongFibonacci(@PathVariable Long num) {
         Mono<String> result = Mono.fromSupplier(() -> String.format("%,d", FibonacciLong.calculate(num)))
-                .subscribeOn(Schedulers.boundedElastic());
+                //.log("getLongFibonacci", Level.INFO, SignalType.ON_SUBSCRIBE, SignalType.ON_NEXT)
+                .subscribeOn(Schedulers.parallel());
         return result.map(ResponseEntity::ok).defaultIfEmpty(ResponseEntity.notFound().build());
     }
 
     @GetMapping(path="/long/random", produces = MediaType.TEXT_PLAIN_VALUE)
-    public Mono<ResponseEntity<String>> getRandom() {
+    public Mono<ResponseEntity<String>> getLongRandomFibonacci() {
         var rnd = new Random().nextLong(40);
         Mono<String> result = Mono.fromSupplier(() -> String.format("%,d", FibonacciLong.calculate(rnd)))
-                .subscribeOn(Schedulers.boundedElastic());
+                //.log("getLongFibonacci", Level.INFO, SignalType.ON_SUBSCRIBE, SignalType.ON_NEXT)
+                .subscribeOn(Schedulers.parallel());
         return result.map(ResponseEntity::ok).defaultIfEmpty(ResponseEntity.notFound().build());
     }
 
     @GetMapping(path="/mono/{num}", produces = MediaType.TEXT_PLAIN_VALUE)
-    public Mono<ResponseEntity<String>> getNonBlockingFibonacci(@PathVariable Long num) {
+    public Mono<ResponseEntity<String>> getMonoFibonacci(@PathVariable Long num) {
         Mono<String> result = FibonacciMono.calculate(num)
                 .map(v -> String.format("%,d", v))
-                .subscribeOn(Schedulers.boundedElastic());
+                //.log("getMonoFibonacci", Level.INFO, SignalType.ON_SUBSCRIBE, SignalType.ON_NEXT)
+                .subscribeOn(Schedulers.parallel());
         return result.map(ResponseEntity::ok).defaultIfEmpty(ResponseEntity.notFound().build());
     }
 
     @GetMapping(path="/mono/random", produces = MediaType.TEXT_PLAIN_VALUE)
-    public Mono<ResponseEntity<String>> getNonBlockingRandom() {
+    public Mono<ResponseEntity<String>> getMonoRandomFibonacci() {
         var rnd = new Random().nextLong(40);
         Mono<String> result = FibonacciMono.calculate(rnd)
                 .map(v -> String.format("%,d", v))
-                .subscribeOn(Schedulers.boundedElastic());
+                //.log("getMonoFibonacci", Level.INFO, SignalType.ON_SUBSCRIBE, SignalType.ON_NEXT)
+                .subscribeOn(Schedulers.parallel());
         return result.map(ResponseEntity::ok).defaultIfEmpty(ResponseEntity.notFound().build());
+    }
+
+    @GetMapping(path="/name/{num}", produces = MediaType.TEXT_PLAIN_VALUE)
+    public Mono<ResponseEntity<String>> getName(@PathVariable int num) {
+        num = num < 1 ? 1 : num;
+        num = num > 1_000_000_000 ? 1_000_000_000 : num;
+
+        return Names.getName(num).map(ResponseEntity::ok).defaultIfEmpty(ResponseEntity.notFound().build());
     }
 }
