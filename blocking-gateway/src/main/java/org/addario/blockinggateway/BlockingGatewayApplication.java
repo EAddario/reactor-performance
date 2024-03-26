d72070a63296c568bf1820a984b305f7da0ba9fa
Ed Addario <ed.addario@paytrix.io>
2024-03-26 21:42:20 +0000

    Add blocking-service

---
+++ blocking-gateway/src/main/java/org/addario/blockinggateway/BlockingGatewayApplication.java
@@ -0,0 +1,13 @@
+package org.addario.blockinggateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlockingGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BlockingGatewayApplication.class, args);
+    }
+
+}
---
+++ blocking-gateway/src/test/java/org/addario/blockinggateway/BlockingGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package org.addario.blockinggateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlockingGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
