259a0e0d998bcbc54493fe9c4d92bff2f06a3fb7
Ed Addario <ed.addario@paytrix.io>
2024-03-26 21:43:04 +0000

    Add reactive-gateway

---
+++ reactive-gateway/src/test/java/org/addario/reactivegateway/ReactiveGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package org.addario.reactivegateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactiveGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
