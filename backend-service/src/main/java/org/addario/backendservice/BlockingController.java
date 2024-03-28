412d93779876cab8aab63f497f46b14f2330ea13
Ed Addario <ed.addario@paytrix.io>
2024-03-28 19:34:10 +0000

    Refactor Mono<ResponseEntity<T>> to ResponseEntity<Mono<T>>

--- backend-service/src/main/java/org/addario/backendservice/BlockingController.java
+++ backend-service/src/main/java/org/addario/backendservice/BlockingController.java
@@ -5,6 +5,7 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
@@ -13,11 +14,13 @@ import java.util.List;
 import java.util.Random;
 
 @RestController
+@RequestMapping("/rest")
 // http://localhost:8081/
 public class BlockingController {
     @GetMapping(path="/blocking/fibonacci/{num}", produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<String> getBlockingFibonacci(@PathVariable Long num) {
         var result = String.format("%,d", BlockingFibonacci.calculate(num));
+
         return new ResponseEntity<>(result, HttpStatus.OK);
     }
 
@@ -25,26 +28,29 @@ public class BlockingController {
     public ResponseEntity<String> getBlockingRandomFibonacci() {
         var rnd = new Random().nextLong(40);
         var result = String.format("%,d", BlockingFibonacci.calculate(rnd));
+
         return new ResponseEntity<>(result, HttpStatus.OK);
     }
 
     @GetMapping(path="/reactive/fibonacci/{num}", produces = MediaType.APPLICATION_JSON_VALUE)
-    public Mono<ResponseEntity<String>> getReactiveFibonacci(@PathVariable Long num) {
-        Mono<String> result = ReactiveFibonacci.calculate(num).map(v -> String.format("%,d", v));
-        return result.map(ResponseEntity::ok).defaultIfEmpty(ResponseEntity.notFound().build());
+    public ResponseEntity<Mono<String>> getReactiveFibonacci(@PathVariable Long num) {
+        var result = ReactiveFibonacci.calculate(num).map(v -> String.format("%,d", v));
+
+        return new ResponseEntity<>(result, HttpStatus.OK);
     }
 
     @GetMapping(path="/reactive/fibonacci/random", produces = MediaType.APPLICATION_JSON_VALUE)
-    public Mono<ResponseEntity<String>> getReactiveRandomFibonacci() {
+    public ResponseEntity<Mono<String>> getReactiveRandomFibonacci() {
         var rnd = new Random().nextLong(40);
-        Mono<String> result = ReactiveFibonacci.calculate(rnd).map(v -> String.format("%,d", v));
-        return result.map(ResponseEntity::ok).defaultIfEmpty(ResponseEntity.notFound().build());
+        var result = ReactiveFibonacci.calculate(rnd).map(v -> String.format("%,d", v));
+
+        return new ResponseEntity<>(result, HttpStatus.OK);
     }
 
     @GetMapping(path="/blocking/name/{num}", produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<String> getBlockingName(@PathVariable int num) {
-        num = num < 1 ? 1 : num;
-        num = num > 1_000_000_000 ? 1_000_000_000 : num;
+        num = Math.max(num, 1);
+        num = Math.min(num, 1_000_000_000);
 
         return new ResponseEntity<>(BlockingNames.getName(num), HttpStatus.OK);
     }
@@ -57,21 +63,19 @@ public class BlockingController {
 
     @GetMapping(path="/reactive/name/{num}", produces = MediaType.APPLICATION_JSON_VALUE)
     public Mono<ResponseEntity<String>> getReactiveName(@PathVariable int num) {
-        num = num < 1 ? 1 : num;
-        num = num > 1_000_000_000 ? 1_000_000_000 : num;
+        num = Math.max(num, 1);
+        num = Math.min(num, 1_000_000_000);
 
         return ReactiveNames.getName(num).map(ResponseEntity::ok).defaultIfEmpty(ResponseEntity.notFound().build());
     }
 
     @GetMapping(path="/reactive/nameslist", produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<Flux<String>> getReactiveNamesList() {
-
         return new ResponseEntity<>(ReactiveNames.getNamesList(), HttpStatus.OK);
     }
 
     @GetMapping(path="/reactive/nameslist/stream", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
     public ResponseEntity<Flux<String>> getReactiveNamesListStream() {
-
         return new ResponseEntity<>(ReactiveNames.getNamesList(), HttpStatus.OK);
     }
 }
