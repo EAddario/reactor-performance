1a46071d706e2ca8cfc11b9ff1935bdccac3a8d5
Ed Addario <ed.addario@paytrix.io>
2024-03-29 12:57:50 +0000

    Add ReactiveRouterConfig class

---
+++ reactive-gateway/src/main/java/org/addario/reactivegateway/ReactiveRouterConfig.java
@@ -0,0 +1,32 @@
+package org.addario.reactivegateway;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.function.server.RouterFunction;
+import org.springframework.web.reactive.function.server.RouterFunctions;
+import org.springframework.web.reactive.function.server.ServerResponse;
+
+@Configuration
+public class ReactiveRouterConfig {
+    private final ReactiveRequestHandler requestHandler;
+
+    public ReactiveRouterConfig(ReactiveRequestHandler requestHandler) {
+        this.requestHandler = requestHandler;
+    }
+
+    @Bean
+    public RouterFunction<ServerResponse> routerFunction() {
+        return RouterFunctions.route()
+                .GET("/blocking/fibonacci/random", requestHandler::blockingRandomFibonacciHandler)
+                .GET("/reactive/fibonacci/random", requestHandler::reactiveRandomFibonacciHandler)
+                .GET("/blocking/fibonacci/{num}", requestHandler::blockingFibonacciHandler)
+                .GET("/reactive/fibonacci/{num}", requestHandler::reactiveFibonacciHandler)
+                .GET("/blocking/name/{num}", requestHandler::blockingNameHandler)
+                .GET("/blocking/nameslist", requestHandler::blockingNamesListHandler)
+                .GET("/reactive/name/{num}", requestHandler::reactiveNameHandler)
+                .GET("/reactive/nameslist", requestHandler::reactiveNamesListHandler)
+                .GET("/reactive/nameslist/stream", requestHandler::reactiveNamesListStreamHandler)
+                .onError(Exception.class, (e, req) -> ServerResponse.badRequest().bodyValue(STR."Error: \{e.getMessage()}"))
+                .build();
+    }
+}
