fef8a788d0d040ff3b955731ac3803d908ec0307
Ed Addario <ed.addario@paytrix.io>
2024-04-03 17:38:02 +0100

    Enable virtual threads and reactor debug agent

--- backend-service/src/main/java/org/addario/backendservice/BackendServiceApplication.java
+++ backend-service/src/main/java/org/addario/backendservice/BackendServiceApplication.java
@@ -2,10 +2,13 @@ package org.addario.backendservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import reactor.tools.agent.ReactorDebugAgent;
 
 @SpringBootApplication
 public class BackendServiceApplication {
     public static void main(String[] args) {
+        System.setProperty("reactor.schedulers.defaultBoundedElasticOnVirtualThreads", "true");
+        ReactorDebugAgent.init();
         SpringApplication.run(BackendServiceApplication.class, args);
     }
 }
