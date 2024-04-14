b735fb2dc0659e5ba74fd0043c3ea0dbb3cf8d69
Ed Addario <ed.addario@paytrix.io>
2024-04-14 22:12:36 +0100

    Enable ReactorDebugAgent and BlockHound

--- backend-service/src/main/java/org/addario/backendservice/BackendServiceApplication.java
+++ backend-service/src/main/java/org/addario/backendservice/BackendServiceApplication.java
@@ -2,13 +2,15 @@ package org.addario.backendservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import reactor.blockhound.BlockHound;
 import reactor.tools.agent.ReactorDebugAgent;
 
 @SpringBootApplication
 public class BackendServiceApplication {
     public static void main(String[] args) {
         System.setProperty("reactor.schedulers.defaultBoundedElasticOnVirtualThreads", "true");
-        ReactorDebugAgent.init();
+        ReactorDebugAgent.init(); // Dev friendly reactive stack traces
+        BlockHound.install(); // Detect and throw on blocking calls from non-blocking threads
         SpringApplication.run(BackendServiceApplication.class, args);
     }
 }
