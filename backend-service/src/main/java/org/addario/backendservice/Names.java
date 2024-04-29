4a7bb1fe9a3bd41f1ed1b7a50f1b6b99e899de9c
Ed Addario <ed.addario@paytrix.io>
2024-04-29 16:07:23 +0100

    Add elements to namesList

--- backend-service/src/main/java/org/addario/backendservice/Names.java
+++ backend-service/src/main/java/org/addario/backendservice/Names.java
@@ -3,9 +3,9 @@ package org.addario.backendservice;
 import java.util.List;
 
 public record Names() {
-    static List<String> namesList = List.of("Ahmad", "Aleksandra", "Ankush", "Anton", "Bahdan", "Bessam",
-            "Bishal", "Chiamaka", "Chris", "Daniel", "Dawid L", "Dawid P", "Douglas", "Ed", "Ekaterina","Elizaveta",
-            "Faris", "Guilherme", "Ian", "Iliyan", "Illia", "Jakub", "Jan", "Jędrzej", "João", "Jonathan", "Leo",
-            "Mark", "Mateusz", "Mic", "Michal I", "Michal Z", "Mikołaj", "Nabeel", "Nazli", "Nicola", "Nkemjika",
-            "Pato", "Pedro", "Rayane", "Reza", "Sudi", "Tuan");
+    static List<String> namesList = List.of("Ahmad", "Aleksandra", "Ankush", "Anton", "Bahdan", "Bessam", "Bishal",
+            "Chiamaka", "Chris", "Daniel", "Darya", "Dawid", "Douglas", "Ed", "Ekaterina", "Elizaveta", "Faris",
+            "Guilherme", "Ian", "Iliyan", "Illia", "Jakub", "Jan", "João", "Jędrzej", "Jonathan", "Leo", "Mark", "Mateusz",
+            "Mic", "Michał I", "Michał Ż", "Mikołaj", "Nabeel", "Nazli", "Nicola", "Nkemjika", "Patricia", "Pedro", "Rayane",
+            "Reza", "Steve", "Sudi", "Tuan");
 }
