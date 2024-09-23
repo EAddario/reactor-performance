7582bc4adaaad5abc84b445a21ee395e91d5b35f
Ed Addario <eaddario@hotmail.com>
2024-09-23 15:04:40 +0100

    Disable BlockHound until library is JDK 23 compatible

--- reactive-gateway/src/main/java/org/addario/reactivegateway/ReactiveGatewayApplication.java
+++ reactive-gateway/src/main/java/org/addario/reactivegateway/ReactiveGatewayApplication.java
@@ -2,14 +2,13 @@ package org.addario.reactivegateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import reactor.blockhound.BlockHound;
 import reactor.tools.agent.ReactorDebugAgent;
 
 @SpringBootApplication
 public class ReactiveGatewayApplication {
     public static void main(String[] args) {
         ReactorDebugAgent.init(); // Dev friendly reactive stack traces
-        BlockHound.install(); // Detect and throw on blocking calls from non-blocking threads
+        //BlockHound.install(); // Detect and throw on blocking calls from non-blocking threads
         SpringApplication.run(ReactiveGatewayApplication.class, args);
     }
 }
